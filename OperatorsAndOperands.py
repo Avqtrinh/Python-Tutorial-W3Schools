48faf8e64f3ffb7e53ff8998223d557dfb5b9f78
Andrew Trinh <atrinh@msudenver.edu>
2018-03-29 19:05:22 -0600

    Python Tutorial Project Innitial commit

---
+++ OperatorsAndOperands.py
@@ -0,0 +1,173 @@
+print('this is a file practicing the syntax of the operators in Python')
+
+print ('6 + 2 is?')
+print (6 + 2)
+
+print()
+print ('6 - 2 is?')
+print (6 - 2)
+
+print()
+print('6*2 is?')
+print(6*2)
+
+print()
+print('6/2 is?')
+print(6/2)
+
+print()
+print('6/5 is?')
+print(6/5)
+
+print()
+print('6mod2 is?')
+print(6%2)
+
+print()
+print('6mod5 is?')
+print(6%5)
+
+print()
+print('2^3 is?')
+print(2**3)
+
+print()
+print('Floor of 6/5 is?')
+print(6//5)
+
+print()
+print ('Does 6 equal 2?')
+print (6==2)
+
+print()
+print ('Does 2 equal 2?')
+print (2==2)
+
+print()
+print ('Does 2 not equal 3?')
+print (2!=2)
+
+print()
+print('3 less than or equal to 3?')
+print(3<=3)
+
+print()
+print('3 less than or equal to 4?')
+print(3<=4)
+
+print()
+print('3 less than or equal to 2?')
+print(3<=2)
+
+print()
+print('3 greater than or equal to 2?')
+print(3>=2)
+
+print()
+print('T or T')
+print(True or True)
+print('T or F')
+print(True or False)
+print('F or F')
+print(False or False)
+
+print()
+print('T and T')
+print(True and True)
+print('T and F')
+print(True and False)
+print('F and F')
+print(False and False)
+
+print()
+print('not T')
+print(not True)
+print('T and not F')
+print(True and not False)
+
+print()
+a=b=20
+c=10
+d=20
+print('a=b=20')
+print('c=10')
+print('a is a')
+print(a is a)
+print('a is b')
+print(a is b)
+print('a is c')
+print(a is c)
+print('a is d')
+print(a is d)
+print('a is not b')
+print(a is not b)
+print('a is not c')
+print(a is not c)
+
+print()
+print('a+=a')
+a += a
+print(a)
+
+print()
+print('a!=a')
+print(a!=a)
+
+
+print()
+a = 8
+b = 4
+print("a: "+ str(a))
+print('b: '+ str(b))
+
+print()
+print('a bianary and b')
+a = a & b
+print(a)
+
+print()
+print('a bianary and a')
+a = a & a
+print(a)
+
+print()
+print('a bianary and a when a = 1')
+a=1
+a = a & a
+print(a)
+
+print()
+print('12 bianary and 6')
+print(12&6)
+
+print()
+print('12 bianary or 6')
+print(12|6)
+
+print()
+print('2 bianary xor 6')
+print(12^6)
+
+
+print()
+a = 8
+print('a: '+ str(a))
+
+print()
+print('a lsr 1')
+a= a>>1
+print(a)
+
+print()
+print('a lsl 1')
+a= a<<1
+print(a)
+
+print()
+print('bianary comp a')
+a=~a
+print(a)
+1000
+
+print()
+print('in and not in need knowledge of some sort of colection or sequence')
