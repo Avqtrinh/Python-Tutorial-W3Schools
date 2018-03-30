55d1f3dc0a17ca1703a8caf9f59e1fb86b3607b7
Andrew Trinh <atrinh@msudenver.edu>
2018-03-29 20:46:40 -0600

    Strings

---
+++ Strings.py
@@ -0,0 +1,20 @@
+ch = "Hello Python"
+str1 = "some string"
+print('first single sub-string is:', ch[0])
+print("Set of sub-string is:",str1[2:8])
+
+print("updated string will be:", ch[:8]+"Python")
+
+
+#some of these no longer work? or maybe i just don't have the assets
+print("here are some special characters")
+print("\a: alert")
+print("\b: backspace")
+print("\cx: Control X")
+print("\e: escape")
+print("\f: Form feed")
+print("\n: New line or next line")
+print("\r: carriage return")
+print("\s: space")
+print("\t: tab")
+print("\v: Vertical Tab")
