f3e2e27f01f014253e636d1355bf17ea817a248a
Avqtrinh <Trinh.Andrew.V@gmail.com>
2018-04-30 13:42:51 -0600

    playing with loop syntax

---
+++ loops.py
@@ -0,0 +1,18 @@
+testList = ["a","b","c",]
+print(testList)
+for x in range (4,8):
+	testList.append(x)
+print(testList)
+
+
+for x in testList:
+	print("At Index ",testList.index(x)," element: " ,x)
+
+x = False
+count = 0
+while x==False:
+	if count == 3:
+		x=True
+	else:	
+		count=count+1
+print count
